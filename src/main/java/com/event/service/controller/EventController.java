c5941a24df3558e7d8afa7855060afba32240c07
dulsara_w <dulsara_w@epiclanka.net>
2025-04-25 23:14:49 +0530

    Code formatted

--- src/main/java/com/event/service/controller/EventController.java
+++ src/main/java/com/event/service/controller/EventController.java
@@ -27,58 +27,56 @@ import javax.naming.ldap.PagedResultsControl;
 @Slf4j
 public class EventController {
 
-  private final MessageSource messageSource;
-  private final EventService eventService;
+    private final MessageSource messageSource;
+    private final EventService eventService;
 
-  @PostMapping(value = EndPoint.CREATE_EVENT , produces = MediaType.APPLICATION_JSON_VALUE)
-  public ResponseEntity<?> createEvent(Locale locale,
-      @RequestBody CreateEventRequest requestBody) {
-    try {
-      //TODO: implement mapper here to map CreateEventRequest to Event and change the service method
-      String eventId = eventService.createEvent(requestBody);
-      if (!eventId.isEmpty() && eventId!=null){
-        return new ResponseEntity<>(eventId, HttpStatus.OK);
-      }else {
-        return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
-      }
+    @PostMapping(value = EndPoint.CREATE_EVENT, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> createEvent(Locale locale,
+                                         @RequestBody CreateEventRequest requestBody) {
+        try {
+            //TODO: implement mapper here to map CreateEventRequest to Event and change the service method
+            String eventId = eventService.createEvent(requestBody);
+            if (!eventId.isEmpty() && eventId != null) {
+                return new ResponseEntity<>(eventId, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+            }
 
-    } catch (ApplicationException e) {
-      ErrorResponse errorResponse = new ErrorResponse(e.getCode(), messageSource.getMessage(e.getMessage(),
-          null, locale));
-      return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+        } catch (ApplicationException e) {
+            ErrorResponse errorResponse = new ErrorResponse(e.getCode(), messageSource.getMessage(e.getMessage(),
+                    null, locale));
+            return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+        }
     }
-  }
 
-  @PutMapping(value = "/events/{eventId}", produces = MediaType.APPLICATION_JSON_VALUE)
-  @PreAuthorize("hasAnyRole('HOST', 'ADMIN')")
-  public ResponseEntity<?> updateEvent(@RequestBody UpdateEvent updateEvent,@PathVariable("eventId") String eventid) throws DataNotFounException {
-    log.info("Updating a task {} ", updateEvent.toString());
-    return eventService.updateTask(updateEvent,eventid);
-  }
-
-
-  @DeleteMapping(value = "/events/{eventId}", produces = MediaType.APPLICATION_JSON_VALUE)
-  @PreAuthorize("hasAnyRole('HOST', 'ADMIN')")
-  public ResponseEntity<?> deleteEvent(@PathVariable("eventId")String eventId) throws DataNotFounException {
-    log.info("Updating a task {} ", eventId.toString());
-    return eventService.deleteEvent(eventId);
-  }
+    @PutMapping(value = "/events/{eventId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @PreAuthorize("hasAnyRole('HOST', 'ADMIN')")
+    public ResponseEntity<?> updateEvent(@RequestBody UpdateEvent updateEvent, @PathVariable("eventId") String eventid) throws DataNotFounException {
+        log.info("Updating a task {} ", updateEvent.toString());
+        return eventService.updateTask(updateEvent, eventid);
+    }
 
 
-  @PostMapping(value = EndPoint.FILTER_EVENT, produces = MediaType.APPLICATION_JSON_VALUE)
-  public List<Event> filterEvents(@RequestBody FilterEvents filterEvents){
-    log.debug("Filtering event details " + filterEvents);
-
-    return eventService.filterEvents(filterEvents);
-  }
-
-
-  @PostMapping(value = EndPoint.UPCOMING_EVENTS, produces = MediaType.APPLICATION_JSON_VALUE)
-  public List<Event> filterEvents(@RequestBody UpcomingEventsRequest upcomingEventsRequest){
-    log.debug("Filtering event details " + upcomingEventsRequest);
-    return eventService.upcomingEvents(upcomingEventsRequest);
-  }
+    @DeleteMapping(value = "/events/{eventId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @PreAuthorize("hasAnyRole('HOST', 'ADMIN')")
+    public ResponseEntity<?> deleteEvent(@PathVariable("eventId") String eventId) throws DataNotFounException {
+        log.info("Updating a task {} ", eventId.toString());
+        return eventService.deleteEvent(eventId);
+    }
 
 
+    @PostMapping(value = EndPoint.FILTER_EVENT, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Event> filterEvents(@RequestBody FilterEvents filterEvents) {
+        log.debug("Filtering event details " + filterEvents);
+
+        return eventService.filterEvents(filterEvents);
+    }
+
+
+    @PostMapping(value = EndPoint.UPCOMING_EVENTS, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Event> filterEvents(@RequestBody UpcomingEventsRequest upcomingEventsRequest) {
+        log.debug("Filtering event details " + upcomingEventsRequest);
+        return eventService.upcomingEvents(upcomingEventsRequest);
+    }
 
 }
